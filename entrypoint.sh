fa30f983b19854611eb9c5cf30732d74799b18e8
NIRAVDEV <101976669+NIRAVDEV@users.noreply.github.com>
2025-06-27 17:09:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Load NVM
 export NVM_DIR="/root/.nvm"
-. "$NVM_DIR/nvm.sh"
+source "$NVM_DIR/nvm.sh"
 nvm use 22
 
-# Install frontend
+# Build frontend
 cd /var/www/mythicaldash/frontend
 yarn install --ignore-engines --force
 yarn build
@@ -14,16 +13,24 @@ yarn build
 cd /var/www/mythicaldash/backend
 composer install --no-interaction --prefer-dist
 
-# Start PHP-FPM and NGINX
+# Start services
 service php8.2-fpm start
 service nginx start
+service redis-server start
 
-# Start backend panel
-php /var/www/mythicaldash/mythicaldash setup
-php /var/www/mythicaldash/mythicaldash migrate
-php /var/www/mythicaldash/mythicaldash pterodactyl configure
-php /var/www/mythicaldash/mythicaldash init
-php /var/www/mythicaldash/mythicaldash makeAdmin
+# Wait for MariaDB to become available
+until mysql -hmariadb -umySQLuser -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;"; do
+  echo "Waiting for MariaDB..."
+  sleep 3
+done
+
+# Setup panel
+cd /var/www/mythicaldash
+php mythicaldash setup
+php mythicaldash migrate
+php mythicaldash pterodactyl configure
+php mythicaldash init
+php mythicaldash makeAdmin
 
 # Keep container running
 tail -f /dev/null
